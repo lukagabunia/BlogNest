113a46c03465d11bfa0d8b38461fd02467455726
luka gabunia <luka68690@gmail.com>
2025-01-04 17:37:09 +0400

    made error notifications for signup inputs

--- js/app.js
+++ js/app.js
@@ -20,13 +20,38 @@ mobileView.addEventListener("click", (event) => {
   nav.classList.toggle("active");
 });
 
-/* // user authorization
-const userAuth = document.querySelector(".auth-input");
-const userPassword = document.querySelector("#password");
-userAuth.addEventListener("input", (e) => {
-  console.log(e);
+const signUpPassword = document.getElementById("pass");
+const signUpConPassword = document.getElementById("con-pass");
+const signUpConEmail = document.getElementById("con-email");
+const signUpEmail = document.getElementById("email");
+const error = document.querySelector(".error");
+const conPassError = document.querySelector(".con-error");
+const conEmailError = document.querySelector(".con-email-error");
+
+signUpPassword.addEventListener("input", (e) => {
+  if (e.target.value.length < 8) {
+    error.style.color = "red";
+    error.style.fontSize = "13px";
+    error.textContent = "Password must be at least 8 characters long";
+  } else {
+    error.textContent = "";
+  }
 });
-if (userPassword.value.length < 8) {
-  userPassword.style.border = "1px solid red";
-}
- */
+signUpConPassword.addEventListener("input", (e) => {
+  if (e.target.value !== signUpPassword.value) {
+    conPassError.style.color = "red";
+    conPassError.style.fontSize = "13px";
+    conPassError.textContent = "Passwords do not match";
+  } else {
+    conPassError.textContent = "";
+  }
+});
+signUpConEmail.addEventListener("input", (e) => {
+  if (e.target.value !== signUpEmail.value) {
+    conEmailError.style.color = "red";
+    conEmailError.style.fontSize = "13px";
+    conEmailError.textContent = "Emails do not match";
+  } else {
+    conEmailError.textContent = "";
+  }
+});
