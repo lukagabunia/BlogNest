ef47df118c5def68aa66bf558d8656736d83e0c7
luka gabunia <luka68690@gmail.com>
2025-01-31 16:10:02 +0400

    fatched data from cards.json and used to create different cards in post.html

--- js/app.js
+++ js/app.js
@@ -43,3 +43,42 @@ if (isLoggedIn) {
   // Show the user image if it exists
   if (userImage) userImage.style.display = "block";
 }
+
+// Load cards
+const postContainer = document.querySelector(".post-container");
+
+fetch("./db/cards.json")
+  .then((response) => response.json())
+  .then((cards) => {
+    cards.forEach((card) => {
+      postContainer.innerHTML += createCard(card);
+    });
+  })
+  .catch((error) => console.error("Error loading cards:", error));
+function createCard(card) {
+  return `
+        <div class="card">
+            <div class="img-container">
+                <img class="card-img w-100 d-block" src="${card.img}" alt="${card.title}" />
+                <img class="fav-icon" src="./Assets/Icons/heart-regular.svg" alt="fav icon" />
+                <span>${card.creation_date}</span>
+            </div>
+            <div class="card-body">
+                <h3>${card.title}</h3>
+                <p>${card.description}</p>
+                <div class="action-container d-flex">
+                    <button class="btn card-btn">
+                        <a href="./post-detail.html?id=${card.id}">See more...</a>
+                    </button>
+                    <img class="share-icon" src="./Assets/Icons/share-solid.svg" alt="share" />
+                </div>
+            </div>
+        </div>
+        `;
+}
+function renderCards(cards) {
+  const postContainer = document.querySelector(".post-container");
+  if (postContainer) {
+    postContainer.innerHTML = cards.map(createCard).join(""); // More efficient rendering
+  }
+}
